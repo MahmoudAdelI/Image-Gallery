bc62251d77185d1ff0b00ec3693b510e9b747dc3
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-11-06 16:41:20 +0200

    removed the link component from the imageContainer

--- src/app/components/ImageContainer.tsx
+++ src/app/components/ImageContainer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import  Image  from 'next/image'
 import type { Photo } from '@/models/Images'
-import Link from 'next/link';
+//import Link from 'next/link';
 import { useState } from 'react';
 
 
